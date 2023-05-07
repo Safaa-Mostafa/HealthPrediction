d2ec6e2a5ddb03ae6dfecd7e3c7f7b1b909a5bd4
Safaa-Mostafa <77861720+Safaa-Mostafa@users.noreply.github.com>
2023-05-07 11:47:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 pip install --upgrade pip 
-
+pip install numpy
+pip install pandas
+pip install pickle4
+pip install joblib
+pip install flask
+pip install h5py
+pip install flask_restful
+pip install scikit-learn
+pip install protobuf==3.20
+pip install Pillow
+pip install matplotlib
+pip install tensorflow==2.1
+pip install keras==2.3.1
 pip install -r requirements.txt
